db301f044fde85f834df72bbb13574878497a078
caleb <calebh101dev@icloud.com>
2026-02-19 10:14:03 -0600

    Start making login widget

--- lib/calebh101_server.dart
+++ lib/calebh101_server.dart
@@ -1,5 +1,14 @@
 /// Support for the Calebh101 server API.
 library;
 
-export 'src/client.dart';
+import 'package:styled_logger/styled_logger.dart';
+
 export 'package:openapi/api.dart';
+
+export 'src/client.dart';
+export 'src/widgets/login.dart';
+
+void enableLogging({bool verbose = true}) {
+  Logger.enable();
+  if (verbose) Logger.enableVerbose();
+}
