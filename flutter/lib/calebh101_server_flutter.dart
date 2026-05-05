0fa5c1f199b4fd13c4aad4153ad73afcf6ea8362
caleb <calebh101dev@icloud.com>
2026-05-04 21:02:25 -0500

    Set auth

--- flutter/lib/calebh101_server_flutter.dart
+++ flutter/lib/calebh101_server_flutter.dart
@@ -1,9 +1,11 @@
-import 'package:styled_logger/styled_logger.dart';
+import 'package:calebh101_server/calebh101_server.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 export 'package:calebh101_server/calebh101_server.dart';
 export 'package:calebh101_server_flutter/src/login.dart';
 
-void calebh101ServerEnableLoggingWidgets({bool verbose = false}) {
-  Logger.enable();
-  if (verbose) Logger.enableVerbose();
+Future<void> setAuth(ApiClient client) async {
+  final prefs = await SharedPreferences.getInstance();
+  final id = prefs.getString("authentication");
+  if (id != null) client.addDefaultHeader("Authentication", id);
 }
